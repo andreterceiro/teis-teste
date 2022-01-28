268c23204f46b97f8a6ef77f55045b4e320f8010
André de Paula Terceiro <andre@terceiro.com.br>
2022-01-28 16:22:20 -0300

    Site

---
+++ javascript.php
@@ -0,0 +1,28 @@
+	<!-- jQuery -->
+	<script  type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	
+	<script type="text/javascript" src="js/roundabout.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+
+	<!-- jQuery Easing -->
+	<script  type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script  type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script  type="text/javascript" src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar -->
+	<script  type="text/javascript" src="js/jquery.stellar.min.js"></script>
+	<!-- Superfish -->
+	<script  type="text/javascript" src="js/hoverIntent.js"></script>
+	<script  type="text/javascript" src="js/superfish.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script  type="text/javascript" src="js/main.js"></script>
+
+	<script>
+		$(function() {
+  		    $(".roundabout-next").attr('right', $(".roundabout-next").attr('right') - 34);
+			$(".roundabout-prev").height(34);
+		});
+	</script>
+
