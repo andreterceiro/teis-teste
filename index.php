1f8276037559fb87b8399e6c5289295f2f578388
André de Paula Terceiro <andre@terceiro.com.br>
2022-01-28 16:54:42 -0300

    feat: agenda -> programação

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2">
 						<div class="heading-section text-center animate-box">
-							<h2>Agenda</h2>
+							<h2>Programação</h2>
 						</div>
 					</div>
 				</div>
